9c86feb87a745bafb978f4306c81972e7be11657
Tide TAM <tide.tam@gmail.com>
2016-05-07 15:16:25 +0800

    version 1.0

---
+++ Assets/_CompletedAssets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace CompleteProject
+{
+	public class GameOverManager : MonoBehaviour
+	{
+		public PlayerHealth playerHealth;
+		Animator anim;
+		public float ShowScoresDelay;
+		float ShowScoresTimer;
+		void Awake()
+		{
+			anim = GetComponent<Animator>();
+			ShowScoresDelay = 50;
+		}
+
+
+		void Update()
+		{
+			// If the player has run out of health...
+			if(playerHealth.currentHealth <= 0) 
+			{
+					anim.SetTrigger ("GameOver");	
+				ShowScoresTimer += Time.deltaTime;
+			}
+		}
+	}
+
+}
---
+++ Assets/_CompletedAssets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,187 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace CompleteProject
+{
+    public class PlayerHealth : MonoBehaviour
+    {
+        public int startingHealth = 100;                            // The amount of health the player starts the game with.
+        public int currentHealth;                                   // The current health the player has.
+        public Slider healthSlider;                                 // Reference to the UI's health bar.
+        public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
+        public AudioClip deathClip;                                 // The audio clip to play when the player dies.
+        public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
+        public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
+
+		int count = 0;
+		int count2 = 0;
+        Animator anim;                                              // Reference to the Animator component.
+        AudioSource playerAudio;                                    // Reference to the AudioSource component.
+        PlayerMovement playerMovement;                              // Reference to the player's movement.
+        PlayerShooting playerShooting;                              // Reference to the PlayerShooting script.
+        bool isDead;                                                // Whether the player is dead.
+        bool damaged;                                               // True when the player gets damaged.
+
+		Rigidbody playerRigidbody; 
+
+		bool triggerBig = false;
+		bool triggerFast = false;
+		public bool isBig;
+		public bool isFast;
+
+        void Awake ()
+        {
+            // Setting up the references.
+            anim = GetComponent <Animator> ();
+            playerAudio = GetComponent <AudioSource> ();
+            playerMovement = GetComponent <PlayerMovement> ();
+            playerShooting = GetComponentInChildren <PlayerShooting> ();
+
+            // Set the initial health of the player.
+            currentHealth = startingHealth;
+
+			playerRigidbody = GetComponent <Rigidbody> ();
+        }
+
+
+        void Update ()
+        {
+			if (triggerBig) {
+				count = 100;
+				triggerBig = false;
+				isBig = true;
+			}
+			if (triggerFast) {
+				count2 = 100;
+				triggerFast = false;
+				isFast = true;
+			}
+
+
+			count--;
+			count2--;
+			if (count < 0) {
+				count = 0;
+				isBig = false;
+			}
+			if (count2 < 0) {
+				count2 = 0;
+				isFast = false;
+			}
+
+			if (isBig) {
+				if (transform.localScale.x < 2.0f) {
+					transform.localScale = Vector3.Lerp (transform.localScale, new Vector3 (2.0F, 2.0F, 2.0F), Time.deltaTime * 5f);
+				} 
+
+			} else {
+				if (transform.localScale.x > 1.0f) {
+					transform.localScale = Vector3.Lerp (transform.localScale, new Vector3 (1.0F, 1.0F, 1.0F), Time.deltaTime * 5f);
+				}
+			}
+
+			if (isFast) {
+				playerMovement.speed = 12f;
+			} else {
+				playerMovement.speed = 6f;
+			}
+
+
+            // If the player has just been damaged...
+            if(damaged)
+            {
+                // ... set the colour of the damageImage to the flash colour.
+                damageImage.color = flashColour;
+            }
+            // Otherwise...
+            else
+            {
+                // ... transition the colour back to clear.
+                damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+            }
+
+            // Reset the damaged flag.
+            damaged = false;
+        }
+
+
+        public void TakeDamage (int amount)
+        {
+            // Set the damaged flag so the screen will flash.
+            damaged = true;
+
+            // Reduce the current health by the damage amount.
+            currentHealth -= amount;
+
+            // Set the health bar's value to the current health.
+            healthSlider.value = currentHealth;
+
+            // Play the hurt sound effect.
+            playerAudio.Play ();
+
+            // If the player has lost all it's health and the death flag hasn't been set yet...
+            if(currentHealth <= 0 && !isDead)
+            {
+                // ... it should die.
+                Death ();
+            }
+        }
+
+
+        void Death ()
+        {
+            // Set the death flag so this function won't be called again.
+            isDead = true;
+
+            // Turn off any remaining shooting effects.
+            playerShooting.DisableEffects ();
+
+            // Tell the animator that the player is dead.
+            anim.SetTrigger ("Die");
+			ScoreBoardManager.AddScore (ScoreManager.score);
+
+            // Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
+            playerAudio.clip = deathClip;
+            playerAudio.Play ();
+
+            // Turn off the movement and shooting scripts.
+            playerMovement.enabled = false;
+            playerShooting.enabled = false;
+        }
+
+
+        public void RestartLevel ()
+        {
+            // Reload the level that is currently loaded.
+
+            SceneManager.LoadScene (0);
+        }
+
+		void OnTriggerEnter(Collider other) 
+		{
+			if (other.gameObject.CompareTag ("healthpack")) {
+				
+				other.gameObject.SetActive (false);
+				if (currentHealth <= 90) {
+					currentHealth += 10;
+				} else {
+					currentHealth = 100;
+				}
+				healthSlider.value = currentHealth;
+			} else if (other.gameObject.CompareTag ("gunpack")) {
+				other.gameObject.SetActive (false);
+				triggerBig = true;
+
+			} else if (other.gameObject.CompareTag ("goldpack")) {
+				other.gameObject.SetActive (false);
+				ScoreManager.score += 30;
+			} else if (other.gameObject.CompareTag ("speedpack")) {
+				other.gameObject.SetActive (false);
+				triggerFast = true;
+			}
+		}
+
+    }
+}
